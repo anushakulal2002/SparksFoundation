a031d640aefdb87e04a2cf5cd9453a9518fa0310
anushakulal2002 <123734582+anushakulal2002@users.noreply.github.com>
2023-06-21 15:14:18 +0530

    Add files via upload

---
+++ bankingWebsite/header.php
@@ -0,0 +1,17 @@
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.php">Home</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"  aria-label="Toggle navigation">
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="transaction.php">Transaction</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="customer.php">View all Customer</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
---
+++ bankingWebsite/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Money Bank</title>
+</head>
+<body>
+<?php require_once 'header.php' ?> 
+
+
+        <section id="services" class="services">
+        <div class="container my-5">
+          <h1 class="text-dark text-center my-5">Welcome to banking Website</h1>
+            <div class="container">
+            <div class="row row-cols-2">       
+              <div class="col ">
+              <h3>Now Manage Everything at one place</h3>   
+                <div class="container col my-5">
+                   <p class="lead"> View all Customers</p>
+                   <a href="customer.php"><button class="btn btn-primary" type="button" >Get Started</button></a>
+                </div>
+                <div class="container col my-5">
+                   <p class="lead"> View all Transaction</p>
+                   <a href="transaction.php"><button class="btn btn-primary" type="button" >Get Started</button></a>
+                </div>
+               
+              </div>
+<br><br>
+              <div class="col">
+              <img src="https://tse4.mm.bing.net/th?id=OIP.nSKLLhhPmfUAtOeJKGiv5wHaEK&pid=Api&P=0&h=180" class="rounded img-fluid float-right" alt="banking">
+              </div>
+            </div>
+            </section>
+
+             
+            </div>
+          </div> 
+</body>
+
+      <!-- bootstrap js script -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+</html>
